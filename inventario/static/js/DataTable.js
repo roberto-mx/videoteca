b2cd73f14cfce0c777eaab9e3255f7bd5c297d34
Mario Israel Jimenez Amaro <MIJIMENEZ@aprende.mx>
2023-03-27 14:03:29 -0600

    Cambios Mario

--- inventario/static/js/DataTable.js
+++ inventario/static/js/DataTable.js
@@ -23,7 +23,7 @@ const generarDataTable = () => {
       "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
       "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
       "sInfoPostFix": "",
-      "sSearch": "Buscar:",
+      "sSearch": "Busca código de barras:",
       "sUrl": "",
       "sInfoThousands": ",",
       "sLoadingRecords": "Cargando...",
@@ -41,6 +41,6 @@ const generarDataTable = () => {
       }
     }
   };
-  
+
   return defaultOptions
 }
