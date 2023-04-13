d00a35a078cd8fe4e94446e4b9aec4e6a2116f41
Mario Israel Jimenez Amaro <MIJIMENEZ@aprende.mx>
2023-04-13 13:23:20 -0600

    Resulto el problema de descarga, sehundo pdf del modal

--- inventario/views/prestamos.py
+++ inventario/views/prestamos.py
@@ -21,7 +21,6 @@ from reportlab.lib.units import inch
 from django.shortcuts import get_object_or_404
 import tempfile
 
-
     # ---------------------------
     # Prestamos
     # ---------------------------
@@ -179,7 +178,7 @@ def generar_pdf(request):
     response.write(pdf.output(dest='S').encode('latin1'))
     return response
 # ---------------------------------PDF2----------------------------------------------------------------------------------------
-@csrf_exempt
+# @csrf_exempt    
 class GENERATE(FPDF):
     def __init__(self, orientation='P', unit='mm', format='A4', q=None):
         super().__init__(orientation, unit, format)
@@ -188,33 +187,33 @@ class GENERATE(FPDF):
     def header(self):
         
         # Configuración de la cabecera del PDF
-        self.image('images/EducaciónAprende.jpeg', x=10, y=8, w=50)
-        self.image('images/logo-aprendemx.png', x=65, y=5, w=50)
+        self.image('images/EducaciónAprende.jpeg', x=10, y=8, w=35)
+        self.image('images/logo-aprendemx.png', x=46, y=7, w=35)
         self.ln()
 
-        self.set_font('Arial', 'B', 8)
-        self.cell(480,1, 'SECRETARÍA DE EDUCACIÓN PÚBLICA', 0, 10, 'C')
+        self.set_font('Arial', 'B', 6)
+        self.cell(323,1, 'SECRETARÍA DE EDUCACIÓN PÚBLICA', 0, 10, 'C')
         self.ln(3)
-        self.cell(440,1, 'Subdirección de Sistematización de Acervos y Desarrollo Audiovisual', 0, 20, 'C')
+        self.cell(293,1, 'Subdirección de Sistematización de Acervos y Desarrollo Audiovisual', 0, 20, 'C')
         self.ln(3)
-        self.cell(518,1, 'Audiovisual', 0, 20, 'C')
+        self.cell(351.5,1, 'Audiovisual', 0, 20, 'C')
         self.ln(3)
-        self.cell(458,1, 'Departamento de Conservación de Acervos Videográficos', 0, 20, 'C')
+        self.cell(306.5,1, 'Departamento de Conservación de Acervos Videográficos', 0, 20, 'C')
         self.ln(40)
 
         self.set_font('Arial', 'B', 8)
         self.cell(84, 10,   'NOMBRE:      _______________________________', 0, 0, 'C')
         # self.ln(7)
-        self.cell(306, 10,  'DIRECCIÓN:   _______________________________', 0, 0, 'C')
+        self.cell(103, 10,  'DIRECCIÓN:   _______________________________', 0, 0, 'C')
         self.ln(17)
         self.cell(83, 10,   'PUESTO:      _______________________________', 0, 0, 'C')
 
         self.cell(103, 10,  'EXTENSIÓN:   _______________________________', 0, 0, 'C')
-        self.cell(103, 10,  'CORREO:      _______________________________', 0, 0, 'C')
+        # self.cell(103, 10,  'CORREO:      _______________________________', 0, 0, 'C')
         self.ln(40)
         
         self.set_font('Arial', 'B', 15)
-        self.cell(280, 10, f'Prestamos de la cinta ({self.q})', 0, 0, 'C')
+        self.cell(180, 10, f'Folio de la cinta ({self.q})', 0, 0, 'C')
         self.ln(20)
         
     def footer2(self):
@@ -239,24 +238,24 @@ class GENERATE(FPDF):
         # Generación del contenido de la tabla en el PDF
         self.set_fill_color(144, 12, 63)
         self.set_text_color(255, 255, 255) # Establece el color de la letra en blanco
-        self.cell(85, 10, 'Código de Barras', 1, 0, '', True)
-        self.cell(85, 10, 'Fecha Prestamo y Devolucón', 1, 0, '', True)
+        self.cell(90, 10, 'Código de Barras', 1, 0, '', True)
+        self.cell(90, 10, 'Fecha Prestamo y Devolucón', 1, 0, '', True)
         self.set_text_color(0, 0, 0)
         self.ln()
 
         for row in data:
-            self.cell(85, 10, str(row['vide_codigo']), 1)
-            self.cell(85, 10, str(row['pres_fecha_devolucion']), 1)
+            self.cell(90, 10, str(row['vide_codigo']), 1)
+            self.cell(90, 10, str(row['pres_fecha_devolucion']), 1)
             self.ln()
 
 def generar_pdf_modal(request):
     q = int(request.GET.get("q"))
     queryset = DetallePrestamos.objects.filter(pres_folio=q).values('vide_codigo', 'pres_fecha_devolucion')
-   
+
     response = HttpResponse(content_type='application/pdf')
     # response['Content-Disposition'] = 'attachment; filename="Videoteca.pdf"'
     response['Content-Disposition'] = f'attachment; filename="Videoteca_{q}.pdf"'
-    pdf = GENERATE('P', 'mm', (300, 350), q)
+    pdf = GENERATE('P', 'mm', (200, 380), q)
 
     # Abre una nueva página en el documento PDF
     pdf.add_page()
@@ -264,7 +263,6 @@ def generar_pdf_modal(request):
     # Agrega los datos de los préstamos a la página actual
     pdf.generate_Table(queryset)
     
-
     response.write(pdf.output(dest='S').encode('latin1'))
     return response
     # return render(request, 'prestamos/prestamos_detalle_list.html', context)
