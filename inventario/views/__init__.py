a72227e128c2a24a4da97f45b9a089ee1948e2fe
ChristianMalv <christianmalvaez@gmail.com>
2023-04-19 13:38:55 -0600

    Update Settings
    To manage media folder

--- inventario/views/__init__.py
+++ inventario/views/__init__.py
@@ -1,4 +1,4 @@
 
 from .maestrocintas import login, DetalleProgramasDeleteView, DetalleProgramasUpdateView, DetalleProgramasCreateView, DetalleProgramasDetailView, DetalleProgramasCreateView, DetalleProgramasUpdateView, DetalleProgramasDetailView, DetalleProgramasCreateView, DetalleProgramasListView, MaestroCintasFormView, MaestroCintasDeleteView, MaestroCintasUpdateView, MaestroCintasDetailView, MaestroCintasCreateView, MaestroCintasListView, FormatosCintasCreateView, FormatosCintasDetailView, FormatosCintasListView, FormatosCintasFormView
-from .prestamos import PrestamosListView, GetFolioPrestamo, GetFolioDetail, RegisterInVideoteca, ValidateOutVideoteca, RegisterOutVideoteca,PrestamoDetalle, Filtrar_prestamos
+from .prestamos import PrestamosListView, GetFolioPrestamo, GetFolioDetail, RegisterInVideoteca, ValidateOutVideoteca, RegisterOutVideoteca,PrestamoDetalle, Filtrar_prestamos, EndInVideoteca
 from .reports import generar_pdf, generar_pdf_modal
--- videoteca/settings.py
+++ videoteca/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -97,7 +97,7 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': 'admin',
         'HOST': 'localhost',
-        'PORT': '5450'
+        'PORT': '5432'
     }
 }
 
@@ -146,3 +146,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGOUT_REDIRECT_URL = "home"
 LOGIN_REDIRECT_URL = "prestamos_list"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
