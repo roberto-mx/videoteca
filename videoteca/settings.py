033f8511a2a06b4e2061dca69d9e40322ac5d88d
ChristianMalv <christianmalvaez@gmail.com>
2023-06-12 10:03:16 -0600

    update settings.py

--- videoteca/settings.py
+++ videoteca/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%kq5yuqph!gf@38qp1yg+!ghk34cr4&5x2krhy3bx)uyfy@tej
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.16.110.29', 'videoteca.aprende.gob.mx', 'localhost', '127.0.0.1']
 
 
 # Application definition
