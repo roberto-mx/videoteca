872745ffceaac6707e4a59764e90ea5b93b92be3
Mario Israel Jimenez Amaro <MIJIMENEZ@aprende.mx>
2023-10-04 18:55:11 -0600

    Cambio de puerto host django

--- videoteca/settings.py
+++ videoteca/settings.py
@@ -75,43 +75,43 @@ WSGI_APPLICATION = 'videoteca.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'videoteca',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    },
-    'users': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'recursoshumanos',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 #         'NAME': 'videoteca',
 #         'USER': 'postgres',
 #         'PASSWORD': 'postgres',
-#         'HOST': '172.16.110.29',  
-#         'PORT': '5432',   
+#         'HOST': 'localhost',
+#         'PORT': '5432'
 #     },
 #     'users': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 #         'NAME': 'recursoshumanos',
 #         'USER': 'postgres',
 #         'PASSWORD': 'postgres',
-#         'HOST': '172.16.110.29', 
-#         'PORT': '5432',  
+#         'HOST': 'localhost',
+#         'PORT': '5432'
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'videoteca',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '172.27.0.1',  
+        'PORT': '5432',   
+    },
+    'users': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'recursoshumanos',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '172.27.0.1', 
+        'PORT': '5432',  
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
