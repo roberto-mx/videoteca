10b2c1c52524cb3ed106c1df52f971c4282a3f12
Mario Israel Jimenez Amaro <MIJIMENEZ@aprende.mx>
2023-10-11 13:58:41 -0600

    Cambios de edición, a bostrap 5.2

--- videoteca/settings.py
+++ videoteca/settings.py
@@ -76,44 +76,44 @@ WSGI_APPLICATION = 'videoteca.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'videoteca',
-#         'USER': 'postgres',
-#         'PASSWORD': 'admin',
-#         'HOST': 'localhost',
-#         'PORT': '5432'
-#     },
-#     'users': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'recursos_humanos',
-#         'USER': 'postgres',
-#         'PASSWORD': 'admin',
-#         'HOST': 'localhost',
-#         'PORT': '5450'
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'videoteca',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',  # Puedes usar "localhost" si la base de datos está en la misma máquina
-        'PORT': '5432',  
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432'
     },
-    # 'users': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'recursos_humanos',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'admin',
-    #     'HOST': 'localhost',
-    #     'PORT': '5450'
-    # }
-    
+    'users': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'recursos_humanos',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5450'
+    }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'videoteca',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'db',  # Puedes usar "localhost" si la base de datos está en la misma máquina
+#         'PORT': '5432',  
+#     },
+#     # 'users': {
+#     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#     #     'NAME': 'recursos_humanos',
+#     #     'USER': 'postgres',
+#     #     'PASSWORD': 'admin',
+#     #     'HOST': 'localhost',
+#     #     'PORT': '5450'
+#     # }
+    
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
