07a77b0414ddb2774033fa9bd2414389f86424fd
Mario Israel Jimenez Amaro <MIJIMENEZ@aprende.mx>
2023-10-20 10:54:58 -0600

    Cambios permisos

--- inventario/middleware.py
+++ inventario/middleware.py
@@ -6,11 +6,9 @@ class GroupRedirectMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-       
         if request.user.is_authenticated:
-            
             if request.user.groups.filter(name='videotecaPermission').exists():
-                # Si el usuario intenta acceder a una ruta diferente a 'prestamos_list',
+                # Redireccionar si no coincide con rutas específicas
                 if (
                     request.path != reverse('prestamos_list') and
                     not request.path.startswith('/prestamos/') and
@@ -20,36 +18,31 @@ class GroupRedirectMiddleware:
                     not request.path.startswith('/prestamos/generate_pdf/') and
                     not request.path.startswith('/prestamos/generate_pdf_modal/') and
                     not request.path.startswith('/prestamos/person_people/') and
-
                     not request.path.startswith('/prestamos/generate_pdf_register/') and
-                    not request.path.startswith('/search_folio/<int:pk>/') and
-                    not request.path.startswith('/detail_folio/<int:pk>/') and
+                    not request.path.startswith('/search_folio/') and
+                    not request.path.startswith('/detail_folio/') and
                     not request.path.startswith('/register_in/') and
                     not request.path.startswith('/validate_out/') and
                     not request.path.startswith('/register_out/') and
                     not request.path.startswith('/end_in/') and
                     not request.path.startswith('/get_report/') and
-
                     not request.path.startswith(reverse('logout'))
                 ):
                     return redirect('prestamos_list')
-
-            # Verificamos si el usuario pertenece al grupo 'calificacion'
             elif request.user.groups.filter(name='calificacion').exists():
-                # si el usuario accede a otra ruta que no sea de calificaciones se le redirige a consulta formulario
+                # Redireccionar si no coincide con rutas específicas
                 if (
-                    request.path != reverse('/consultaFormulario/') and
-                    not request.path.startswith('/calificaciones/agregar/programa/<str:codigo_barras>') and
-                    not request.path.startswith('/calificaciones/editar/<int:id>/<str:codigo_barras>/') and
-                    not request.path.startswith('/calificaciones/editar/programa/<int:programa_id>/') and
-                    not request.path.startswith('/calificaciones/eliminar/<int:id>/') and
-                    not request.path.startswith('/calificaciones/eliminarRegistro/<int:id>/') and
-                    not request.path.startswith('/calificaciones/cambiarEstatusCalificacion/<int:id>/') and
-                    request.path != reverse('formulario') and
+                    request.path != reverse('consultaFormulario') and
+                    not request.path.startswith('/calificaciones/agregar/programa/') and
+                    not request.path.startswith('/calificaciones/editar/') and
+                    not request.path.startswith('/calificaciones/editar/programa/') and
+                    not request.path.startswith('/calificaciones/eliminar/') and
+                    not request.path.startswith('/calificaciones/eliminarRegistro/') and
+                    not request.path.startswith('/calificaciones/cambiarEstatusCalificacion/') and
+                    not request.path == reverse('formulario') and
                     not request.path.startswith(reverse('logout'))
                 ):
                     return redirect('consultaFormulario')
 
         response = self.get_response(request)
         return response
-
