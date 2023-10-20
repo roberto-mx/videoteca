9f184980fce0c772368a154bbf514b665f16b256
Mario Israel Jimenez Amaro <MIJIMENEZ@aprende.mx>
2023-10-19 18:59:20 -0600

    middleware

--- videoteca/settings.py
+++ videoteca/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'inventario.middleware.GroupRedirectMiddleware', 
+   # 'inventario.middleware.GroupRedirectMiddleware', 
 ]
 
 ROOT_URLCONF = 'videoteca.urls'
