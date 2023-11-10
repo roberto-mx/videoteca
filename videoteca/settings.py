cbbef3b10f14b231fcb89d1c56359d91af9db6dd
Mario Israel Jimenez Amaro <MIJIMENEZ@aprende.mx>
2023-11-10 11:58:00 -0600

    Confirmación de cambios para obtener base de produccion

--- videoteca/settings.py
+++ videoteca/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'db',
-        'PORT': '5430'
+        'PORT': '5431'
     },
     'users': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -92,7 +92,7 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'rh',
-        'PORT': '5433'
+        'PORT': '5430'
     }
 }
 
